e92fa84cb59d071dc86f57d6e9bac5ef3f532750
Leigh966 <leigh966@hotmail.com>
2023-01-12 22:39:23 +0000

    updated test script to handle new endpoint name

--- test/main.py
+++ test/main.py
@@ -5,11 +5,11 @@ import glob
 API_URL = 'http://localhost:5000'
 TEST_DATA_FOLDER = 'data'
 
-def send_file(filename):
+def convert_file(filename):
     path = os.path.join(TEST_DATA_FOLDER, filename)
     print("opening "+path)
     with open(path, 'rb') as f:
-        r = requests.post(API_URL, files={'file': f})
+        r = requests.post(API_URL+"/mp4ToGif", files={'file': f})
         assert(r.status_code == 200)
         print(f'TEST PASSED: Successfully converted "{filename}" to .gif')
 
@@ -18,4 +18,4 @@ os.chdir(TEST_DATA_FOLDER)
 my_files = glob.glob('*.mp4')
 os.chdir("..")
 for file in my_files:
-    send_file(file)
+    convert_file(file)
