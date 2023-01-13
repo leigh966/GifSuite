d3724788936ac07d96769d8a52567cfd13678f70
Leigh966 <leigh966@hotmail.com>
2023-01-13 17:07:52 +0000

    Added another, bigger testcase for the get_sub_binary method. all tests still passing

--- API/test/binary_manipulation.py
+++ API/test/binary_manipulation.py
@@ -3,7 +3,7 @@ import unittest
 from parameterized import parameterized
 
 class TestBinaryManipulation(unittest.TestCase):
-    @parameterized.expand([[8, 0, 3, 0], [10, 0, 2, 2], [10, 2, 4, 2]])
+    @parameterized.expand([[8, 0, 3, 0], [10, 0, 2, 2], [10, 2, 4, 2], [246, 4, 7, 7]])
     def test_get_sub_binary(self, bin, start, end, expected):
         out = bm.get_sub_binary(bin, start, end)
         self.assertEqual(expected, out)
