15e40b78636a9e312b26525d7f8b4f8d6da330d2
Leigh966 <leigh966@hotmail.com>
2023-01-13 15:04:47 +0000

    reading in height and width with tests for these values

--- API/Gif.py
+++ API/Gif.py
@@ -1,6 +1,8 @@
 class Gif:
     def __init__(self, gif_bytes):
         self.version = gif_bytes[3:6]
+        self.width = int.from_bytes(gif_bytes[7:5:-1])
+        self.height = int.from_bytes(gif_bytes[9:7:-1])
 
     @staticmethod
     def read_from_file(path):
--- API/test/TestGif.py
+++ API/test/TestGif.py
@@ -1,5 +1,6 @@
 from API.Gif import Gif
 import unittest
+from parameterized import parameterized
 
 
 class TestGif(unittest.TestCase):
@@ -19,6 +20,17 @@ class TestGif(unittest.TestCase):
         auto = Gif.read_from_file(path)
         self.assertEqual(auto.version, manual.version, "Using read_from_file should yield the " +
                                                        "same results as manually reading and passing in bytes")
+
+
+    @parameterized.expand([["data/tsTimer.gif", 240]])
+    def test_height_correct(self, file_path, expected_height):
+        gif = Gif.read_from_file(file_path)
+        self.assertEqual(expected_height, gif.height)
+
+    @parameterized.expand([["data/tsTimer.gif", 426]])
+    def test_width_correct(self, file_path, expected_width):
+        gif = Gif.read_from_file(file_path)
+        self.assertEqual(expected_width, gif.width)
 
 
 
