70412b96d4516a5ee7b28c66b7255c4e7acf8272
Leigh966 <leigh966@hotmail.com>
2023-01-13 19:47:33 +0000

    changed testing for main to use the unittest library

--- API/test/main.py
+++ API/test/main.py
@@ -1,21 +1,22 @@
 import os.path
 import requests
 import glob
+import unittest
 
 API_URL = 'http://localhost:5000'
 TEST_DATA_FOLDER = 'data'
-
 def convert_file(filename):
     path = os.path.join(TEST_DATA_FOLDER, filename)
-    print("opening "+path)
     with open(path, 'rb') as f:
-        r = requests.post(API_URL+"/mp4ToGif", files={'file': f})
-        assert(r.status_code == 200)
-        print(f'TEST PASSED: Successfully converted "{filename}" to .gif')
+        r = requests.post(API_URL + "/mp4ToGif", files={'file': f})
+        assert (r.status_code == 200)
 
 
-os.chdir(TEST_DATA_FOLDER)
-my_files = glob.glob('*.mp4')
-os.chdir("..")
-for file in my_files:
-    convert_file(file)
+class TestMain(unittest.TestCase):
+    def test_mp4ToGif_endpoint(self):
+        os.chdir(TEST_DATA_FOLDER)
+        my_files = glob.glob('*.mp4')
+        os.chdir("..")
+        for file in my_files:
+            with self.subTest(filename=file):
+                convert_file(file)
