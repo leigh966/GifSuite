e5ae57c383ad73f86f743fa14474136fcc2f1b29
Leigh966 <leigh966@hotmail.com>
2023-01-13 14:10:05 +0000

    Created read_from_file method for Gif to read in a gif from secondary storage and return a gif object

--- API/Gif.py
+++ API/Gif.py
@@ -1,3 +1,9 @@
 class Gif:
     def __init__(self, gif_bytes):
         self.version = gif_bytes[3:6]
+
+    @staticmethod
+    def read_from_file(path):
+        with open(path, 'rb') as f:
+            contents = f.read()
+            return Gif(contents)
--- API/test/TestGif.py
+++ API/test/TestGif.py
@@ -10,6 +10,17 @@ class TestGif(unittest.TestCase):
             b = bytes("89a", 'utf-8')
             self.assertEqual(b, my_gif.version)
 
+    def test_read_from_file(self):
+        manual = ()
+        path = "data/tsTimer.gif"
+        with open(path, 'rb') as f:
+            contents = f.read()
+            manual = Gif(contents)
+        auto = Gif.read_from_file(path)
+        self.assertEqual(auto.version, manual.version, "Using read_from_file should yield the " +
+                                                       "same results as manually reading and passing in bytes")
+
+
 
 if __name__ == '__main__':
     unittest.main()
