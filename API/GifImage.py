63b706370dc7cb6f1ceb6b1325fcc250ddaecb8f
leigh966 <leigh966@hotmail.com>
2023-01-16 18:43:49 +0000

    Created methods in GifImage for getting booleans representing whether the image is interlaced and whether it defines a local color map as well as a method to get the bits per pixel of the image. Also created passing tests for all of these methods

--- API/GifImage.py
+++ API/GifImage.py
@@ -1,9 +1,33 @@
+from API.binary_manipulation import get_bit, get_sub_binary
+
 class GifImage:
     def __init__(self, descriptor_bytes):
         self.__left = int.from_bytes(descriptor_bytes[2:0:-1],"big")
         self.__top = int.from_bytes(descriptor_bytes[4:2:-1],"big")
         self.__width = int.from_bytes(descriptor_bytes[6:4:-1],"big")
         self.__height = int.from_bytes(descriptor_bytes[8:6:-1],"big")
+        self.__mip_byte = descriptor_bytes[9]
+
+    __bits_per_pixel = None
+
+    def get_bits_per_pixel(self):
+        if self.__bits_per_pixel is None:
+            self.__bits_per_pixel = get_sub_binary(self.__mip_byte, 0, 3)
+        return self.__bits_per_pixel+1
+
+    __is_interlaced = None
+
+    def get_is_interlaced(self):
+        if self.__is_interlaced is None:
+            self.__is_interlaced = get_bit(self.__mip_byte, 6)
+        return self.__is_interlaced
+
+    __has_local_color_map = None
+
+    def get_has_local_color_map(self):
+        if self.__has_local_color_map is None:
+            self.__has_local_color_map = get_bit(self.__mip_byte, 7)
+        return self.__has_local_color_map
 
     def get_left(self):
         return self.__left
--- API/test/TestGifImage.py
+++ API/test/TestGifImage.py
@@ -4,12 +4,22 @@ from parameterized import parameterized
 
 
 class TestGifImage(unittest.TestCase):
-    @parameterized.expand([[b',\x00\x00\x00\x00\xaa\x01\xf0\x00', 0, 0, 426, 240]])
+    @parameterized.expand([[b',\x00\x00\x00\x00\xaa\x01\xf0\x00\x00', 0, 0, 426, 240]])
     def test_image_placement(self, input_bytes, expected_left, expected_top, expected_width, expected_height):
         gif_image = GifImage(bytes(input_bytes))
         names = ["left", "top", "width", "height"]
         expecteds = [expected_left, expected_top, expected_width, expected_height]
         actuals = [gif_image.get_left(), gif_image.get_top(), gif_image.get_width(), gif_image.get_height()]
+        for i in range(0, len(names)):
+            with self.subTest(name=names[i]):
+                self.assertEqual(actuals[i], expecteds[i])
+
+    @parameterized.expand([[b',\x00\x00\x00\x00\x00\x00\x00\x00\xc7', 8, True, True]])
+    def test_mip_byte_values(self, input_bytes, expected_bits_per_pixel, expected_is_interlaced, expected_has_local_map):
+        gif_image = GifImage(bytes(input_bytes))
+        names = ["bits per pixel", "interlaced", "has local map"]
+        expecteds = [expected_bits_per_pixel, expected_is_interlaced, expected_has_local_map]
+        actuals = [gif_image.get_bits_per_pixel(), gif_image.get_is_interlaced(), gif_image.get_has_local_color_map()]
         for i in range(0, len(names)):
             with self.subTest(name=names[i]):
                 self.assertEqual(actuals[i], expecteds[i])
